3af2cfa1fb4ebc03adcedfe00b8122e5a91e9abe
Shota Narisaka <shootacean@gmail.com>
2022-06-16 21:50:48 +0900

    feat(CountdownTimer): リロードしてもタイマーがリセットされないようにする

--- __tests__/components/CountdownTimer.test.tsx
+++ __tests__/components/CountdownTimer.test.tsx
@@ -2,11 +2,14 @@ import { render, screen } from '@testing-library/react';
 import { CountdownTimer } from '@/components/CountdownTimer';
 
 describe('CountdownTimer', () => {
-  it('初期状態は 00:00:00 になっている', () => {
+  test('初期状態は 00:00:00 になっている', () => {
     render(<CountdownTimer />);
     const text = screen.getByRole('heading', {
       name: '00:00:00',
     });
     expect(text).toBeInTheDocument();
   });
+  test.todo('過去の日時を指定した場合、タイマーは開始されない');
+  test.todo('タイマー開始後、カウントダウンされている');
+  test.todo('リロード時もタイマーが維持されている');
 });
--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -7,19 +7,31 @@ import {
   formatTimeString,
 } from '@/libs/lib';
 
-const today = new Date();
+let initDate = new Date();
 
 export type CountdownTimerProps = {};
 
 export const CountdownTimer = ({}: CountdownTimerProps) => {
-  const [date, setDate] = useState(formatDateString(today));
-  const [time, setTime] = useState(formatTimeString(today));
+  const [date, setDate] = useState(formatDateString(initDate));
+  const [time, setTime] = useState(formatTimeString(initDate));
+
+  useEffect(() => {
+    // 永続化された指定日時を復元する(初回のみ実行)
+    const persistenceDate = localStorage.getItem('persistenceDate');
+    const persistenceTime = localStorage.getItem('persistenceTime');
+    if (persistenceDate && persistenceTime) {
+      setDate(persistenceDate);
+      setTime(persistenceTime);
+    }
+  }, []);
 
   const { seconds, minutes, hours, days, pause, restart } = useTimer({
     expiryTimestamp: new Date(`${date}T${time}:00`),
+    // TODO タイマー終了時は通知する
     onExpire: () => console.info('onExpire called'),
     autoStart: false,
   });
+
   useEffect(() => {
     // 残り時間をタイトルに表示する
     // TODO CountdownTimerコンポーネントではなくpagesで更新するように変更する
@@ -30,12 +42,17 @@ export const CountdownTimer = ({}: CountdownTimerProps) => {
     if (new Date(`${date}T${time}:00`) <= new Date()) {
       // 過去の日付の場合はタイマーをリセットする
       pause();
-      restart(new Date(`${date}T${time}:00`), false);
+      // FIXME restart()は不要かもしれない
+      // restart(new Date(`${date}T${time}:00`), false);
     } else {
       // 未来の日付ならタイマーを開始する
       restart(new Date(`${date}T${time}:00`), true);
+      // 指定日時を永続化する
+      // FIXME リロード時に2回走ってしまっている
+      localStorage.setItem('persistenceDate', date);
+      localStorage.setItem('persistenceTime', time);
     }
-  }, [date, time]);
+  }, [date, time]); // TODO ESLintのエラーを根本的に解決する
 
   return (
     <>
